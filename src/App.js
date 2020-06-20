47c49f4831bd63951d82cd8f766cfcee548af1a3
Bruno Henrique <bruno@cbkdigital.com.br>
2020-06-20 08:29:55 -0400

    feature: Diversas alteraçoes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [repository, setRepository] = useState({ title: "" });
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    if (repository.title.length < 1) return;
+
+    api.post("/repositories", repository).then((response) => {
+      setRepositories([...repositories, response.data]);
+      setRepository({ title: "" });
+    });
   }
 
   async function handleRemoveRepository(id) {
@@ -14,15 +29,22 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
+      <input
+        onChange={(e) =>
+          setRepository({ ...repository, title: e.target.value })
+        }
+        value={repository.title}
+        autoFocus
+        type="text"
+      />
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
